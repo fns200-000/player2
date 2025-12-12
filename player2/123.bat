31f92327736e434a0ebe5493b7e4825061f37775
fns200-000 <felipe880333999@gmail.com>
2025-12-12 18:26:08 -0300

    ei aqui
    Por favor, abaste tudo ou se colocar em uma pasta, bote todos os arquivos na mesma pasta.

---
+++ player2/123.bat
@@ -0,0 +1,18 @@
+@echo off
+set "wait= timeout /t 99 > nul
+mode 57,4
+title erro do sistema
+color 0c
+echo.
+echo ERRO FATAL: O sistema encontrou um problema inesperado.
+echo.
+%wait%
+%wait%
+%wait%
+%wait%
+%wait%
+%wait%
+%wait%
+%wait%
+%wait%
+pause
